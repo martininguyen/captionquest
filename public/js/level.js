73fe72116b8955916b0841c78650a2a9cd239fc7
Kimberly Ly <kimberly.ly95@gmail.com>
2016-02-11 21:21:43 -0800

    Upload preview test

--- public/js/level.js
+++ public/js/level.js
@@ -1,6 +1,23 @@
-$(document).ready(function() {
-	$('#fileUpload').hide();
-});
+
 $('.upload').click(function() {
 	$('#fileUpload').trigger('click');
+});
+
+function readURL(input) {
+	console.log("hello");
+	console.log(input.files);
+	console.log(input.files[0]);
+	if (input.files && input.files[0]) {
+		console.log("hi????");
+		var reader = new FileReader();
+		reader.onload = displaySuccess;
+
+		function displaySuccess(e) {
+			console.log(e.target.result);
+			$('#imageWrapper').attr('body-background-image: url("' + e.target.result + '")');
+		}
+	}
+}
+$('#fileUpload').change(function() {
+	readURL(this);
 });
